abe2deab3f22246964973587dc26164687a15cdb
Eric Shull <eshulldesign@gmail.com>
2018-12-31 14:16:46 -0800

    add initial files and login screen for app

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/main.m \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/Shull/Desktop/repos/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
